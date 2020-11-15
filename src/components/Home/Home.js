d52ce4bf7556ad39ff86f6155b131a339db4a813
Ayush Pandey <pandey@Ayushs-MacBook-Air.local>
2020-11-15 16:51:38 +0100

    Entering the game logic and Home screen

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { Form, Button } from "react-bootstrap";
+import { useState } from "react";
 import "./Home.scss";
 function Home() {
+  const [gameId, setGameId] = useState();
+  function enterGame(event) {
+    console.log("Entering the game with ID:::" + gameId);
+    event.preventDefault();
+  }
+  function changeGameId(event) {
+    console.log(event.target.value);
+    setGameId(event.target.value);
+  }
   return (
-    <div>
+    <div className="home_container">
       <h1 className="main_title">Hues and Colors</h1>
+      <Form className="gameID_form" onSubmit={enterGame}>
+        <Form.Group>
+          <Form.Control
+            type="text"
+            placeholder="Enter the Game ID"
+            maxLength="5"
+            onChange={changeGameId}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
